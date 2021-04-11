9cfb0431477a4b7719f275fde326f9298e967f04
Sam Sweigart <samuel.sweigart@gmail.com>
2021-04-11 10:43:31 -0400

    reduces api call to single variable using async/await/fetch

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,6 +3,7 @@ console.log('js connected');
 let userSearch = $('#user-search');
 let searchBtn = $('#search-btn');
 let searchResults = $('#search-results');
+let currentForecast = $('#current-forecast');
 let ulEl = $('<ul>');
 let liEl = $('<li>');
 // use moment.js to get current Day/Date/Year/Time
@@ -11,12 +12,11 @@ let getDate = moment().format('LLLL');
 $(document).ready(() => {
     // use async await to avoid needing to use mulitple promise chains
     // allows for more concise syntax 😍 
-    const getCoords = async(cityInput) => {
+    const getCoords = async (cityInput) => {
         // concat cityInput w/ obj literal to url query
         const coordsRequest = `http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=84d61ff029585a95fbd34cf405a10229`;
-        //use await to wait for a promise so rest of page can load while 
-        const coordsResponse = await fetch(coordsRequest);
-        const coordsData = await coordsResponse.json();
+        // combined varibales for single variable call I enjoy this syntax
+        const coordsData = await (await fetch(coordsRequest)).json();
         let coords = coordsData.coord;
         let coordsObj = {
             lat: coords.lat,
@@ -25,11 +25,11 @@ $(document).ready(() => {
         console.log(coordsObj);
 
         const oneCallRequest = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordsObj.lat}&lon=${coordsObj.lon}&units=imperial&exclude=minutely,hourly,daily&appid=84d61ff029585a95fbd34cf405a10229`;
-        const oneCallResponse = await fetch(oneCallRequest);
-        const oneCallData = await oneCallResponse.json();
+        const oneCallData = await (await fetch(oneCallRequest)).json();
         // deconstruct 'oneCallKey' to access the 'current' key in oneCallData
         const oneCallKey = oneCallData.current;
-        console.log(oneCallKey)
+        console.log(oneCallKey);
+
         const oneCallObj = {
             windSpeed: oneCallKey.wind_speed,
             humidity: oneCallKey.humidity,
@@ -51,11 +51,18 @@ $(document).ready(() => {
     // ON CLICK search button event
     // get userSearch value for coordsRequest API
     searchBtn.on('click', () => {
+        // format user input for url query 
+        // make userSearch value all lowercase and trim any white space 
         let userInput = $(userSearch).val().toLowerCase().trim();
-        console.log(userInput);
+        // take userInput and capitalize first character for display
+        userResults = userInput.charAt(0).toUpperCase().concat(userInput.slice(1));
+
+        console.log(userResults);
         // add class & append addChild <li>
         searchResults.after((ulEl).addClass('list-group list-group-flush').append(liEl));
-        liEl.addClass('list-group-item pl-1').text(userInput);
+        liEl.addClass('list-group-item pl-1').text(userResults);
+        // use toUpperCase on [0] index
+        currentForecast.text(userResults);
         //pass userInput to getCoords function and pass cityInput param into coordsRequest url query
         getCoords(userInput);
     });
