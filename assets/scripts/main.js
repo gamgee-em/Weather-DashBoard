7848b6c32b7a7b181cb8cd83dc901d9187d8bfec
Sam Sweigart <samuel.sweigart@gmail.com>
2021-04-11 21:44:58 -0400

    polished styling & added cards for 5 day forecast

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,8 +4,10 @@ let userSearch = $('#user-search');
 let searchBtn = $('#search-btn');
 searchBtn.addClass('bg-secondary').html('Search');
 let currentForecast = $('#current-forecast');
+// SEARCH RESULT LIST
+let searchResults = $('#search-results');
 let globalWeatherObj;
-let globalOneCallObj;
+let globaloneCallCurrentObj;
 let ulEl = $('<ul>');
 let liEl = $('<li>');
 // use moment.js to get current Day/Date/Year/Time
@@ -13,6 +15,10 @@ let getDate = moment().format('LLLL');
 console.log(getDate)
 // doc ready function
 $(document).ready(() => {
+    searchResults.append($('<ul>').addClass('list-group list-group-flush saved-search'));
+    
+    $('.saved-search').append($('<p>').addClass('list-group-item pl-1 city-name rounded'));
+    $('.city-name').html('Search Results:');
 
     // use async await to avoid needing to use mulitple promise chains
     // allows for more concise syntax 😍 
@@ -28,75 +34,79 @@ $(document).ready(() => {
         };
         console.log(coordsObj);
 
-        const oneCallRequest = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordsObj.lat}&lon=${coordsObj.lon}&units=imperial&exclude=minutely,hourly,daily&appid=84d61ff029585a95fbd34cf405a10229`;
+        const oneCallRequest = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordsObj.lat}&lon=${coordsObj.lon}&units=imperial&exclude=minutely,hourly&appid=84d61ff029585a95fbd34cf405a10229`;
         const oneCallData = await (await fetch(oneCallRequest)).json();
-        // deconstruct 'oneCallKey' to access the 'current' key in oneCallData
-        const oneCallKey = oneCallData.current;
-        console.log(oneCallKey);
-
-        const oneCallObj = {
-            windSpeed: oneCallKey.wind_speed,
-            humidity: oneCallKey.humidity,
-            temp: oneCallKey.temp,
-            uvi: oneCallKey.uvi
+        // deconstruct 'oneCallCurrentKey' to access the 'current' key in oneCallData
+        const oneCallCurrentKey = oneCallData.current;
+        console.log(oneCallData);
+        const oneCallCurrentObj = {
+            windSpeed: oneCallCurrentKey.wind_speed,
+            humidity: oneCallCurrentKey.humidity,
+            temp: oneCallCurrentKey.temp,
+            uvi: oneCallCurrentKey.uvi
         };
-        globalOneCallObj = oneCallObj;
-        console.log(oneCallObj);
+        globaloneCallCurrentObj = oneCallCurrentObj;
+        console.log(oneCallCurrentObj);
 
-        // deconstruct 'oneCallKey' object to access 'weather' key in OneCallKey
-        const weatherKey = oneCallKey.weather[0];
+        const oneCallDailyKey = oneCallData.daily;
+        const oneCallDailyObj = {
+            humidity: oneCallDailyKey[0].humidity,
+            temp: oneCallDailyKey[0].temp,
+            uvi: oneCallDailyKey[0].uvi
+        };
+        console.log(oneCallDailyObj.temp);
+
+        // deconstruct 'oneCallCurrentKey' object to access 'weather' key in oneCallCurrentKey
+        const weatherKey = oneCallCurrentKey.weather[0];
         const weatherObj = {
             description: weatherKey.description,
             icon: weatherKey.icon,
             main: weatherKey.main
         };
         globalWeatherObj = weatherObj;
-        globalOneCallObj = oneCallObj;
-        console.log(globalOneCallObj)
+        globaloneCallCurrentObj = oneCallCurrentObj;
+        console.log(globaloneCallCurrentObj)
 
         renderPage()
     };
 
-     function renderPage() {
-        // add class & append <li>
-        let searchResults = $('#search-results');
-
-        searchResults.append($('<ul>').addClass('list-group list-group-flush saved-search'));
+    // build html
+    function renderPage() {
         //save value to localstorage later on
-        $('.saved-search').prepend($('<li>').addClass('list-group-item pl-1 city-name btn bg-secondary rounded').html(userResults));
+        // LIST ITEMS
+        $('.city-name').append($('<li>').addClass('list-group-item pl-1 city-name btn bg-secondary rounded').html(userResults));
         // use toUpperCase on [0] index
         currentForecast.append($('<ul>').attr({
             id: 'current-forecast-list',
             class: 'list-group list-group-flush'
         }));
 
-        // recreate current weather card from dom
-        /* $('.search-area').append($('<div>').attr('class')) */
-
-        $('#current-forecast-list').append($('<h4>').attr('class','list-group-item pl-1 date').html(getDate));
-
-        $('#current-forecast-list').append($('<li>').attr('class', 'list-group-item pl-1 image').html(`${userResults}`));
-        // add icon need to fix position later
+        //current weather card
+        $('.current-weather-card').prepend($('<div>').attr('class','card-body card col-12 current-weather-items'));
+        $('.current-weather-items').append($('<h4>').attr('class','date').html(getDate));
+        //add city to card
+        $('.date').after($('<h6>').attr('class', 'pl-1 image').html(`${userResults}`));
+        // add icon to card
         $('.image').append($('<img>').attr({
             class: 'current-weather-icon',
             src: `https://openweathermap.org/img/wn/${globalWeatherObj.icon}.png`,
             alt: 'Weather icon representing the current weather'
         }));
-
-        $('.current-weather-icon').after($('<p>').attr('class','pl-1').html(`Temp: ${globalOneCallObj.temp} <br> Humidity: ${globalOneCallObj.humidity}% <br> Wind Speed: ${globalOneCallObj.windSpeed}mph <br> UV Index: ${globalOneCallObj.uvi}`));
+        // add weather elements to card
+        $('.image').after($('<p>').attr('class','pl-1').html(`Temp: ${globaloneCallCurrentObj.temp}&degF <br> Humidity: ${globaloneCallCurrentObj.humidity}% <br> Wind Speed: ${globaloneCallCurrentObj.windSpeed}mph <br> UV Index: ${globaloneCallCurrentObj.uvi}`));
 
     }
-   // ON CLICK search button event
-    // get userSearch value for coordsRequest API
-    searchBtn.on('click', () => {
-        // format user input for url query 
-        // make userSearch value all lowercase and trim any white space 
-        let userInput = $(userSearch).val().toLowerCase().trim();
-        // take userInput and capitalize first character for display
-        userResults = userInput.charAt(0).toUpperCase().concat(userInput.slice(1));
-        // call getCoords and pass userInput in as a parameter
-        getCoords(userInput);
-    });
+    // ON CLICK search button event
+        // get userSearch value for coordsRequest API
+        searchBtn.on('click', () => {
+            // format user input for url query 
+            // make userSearch value all lowercase and trim any white space 
+            let userInput = $(userSearch).val().toLowerCase().trim();
+            // take userInput and capitalize first character for display
+            userResults = userInput.charAt(0).toUpperCase().concat(userInput.slice(1));
+            // call getCoords and pass userInput in as a parameter
+            getCoords(userInput);
+        });
     
     function getLocalFunc() {
 
